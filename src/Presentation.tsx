744f6f2f02d8e52576b4342ff078faeb21734473
Hal <haln_01@proton.me>
2025-04-06 18:26:11 -0700

    added styling to the page

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,37 +1,59 @@
-import { Typography } from "@mui/material";
-import { homePageDescription } from "./utils/constants";
+import { Typography, Box } from "@mui/material";
 
 const videoUrl: string = "https://csc466bucket.haln.dev/presentation-final.mp4";
 
 export function Presentation() {
-  return (
-    <>
-      <div className="home-page-content-wrapper">
-        <div className="home-page-content">
-          <div className="home-page-site-title">
-            <Typography
-              variant="h6"
-              component="a"
-              align="center"
-              sx={{
-                mr: 2,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-                fontSize: 30,
-              }}
-            ></Typography>
-          </div>
-          <p className="home-page-site-description">{homePageDescription}</p>
+	return (
+		<main className="home-page-content">
+			<Box
+				width="100%"
+				mb="3em"
+				mt="10em"
+				display="flex"
+				flexDirection="column"
+				flex="center"
+				gap="1.5rem"
+			>
+				<Typography
+					variant="h6"
+					align="center"
+					mx="auto"
+					sx={{
+						fontFamily: "monospace",
+						fontWeight: 700,
+						letterSpacing: ".3rem",
+						color: "inherit",
+						textDecoration: "none",
+						fontSize: 30,
+					}}
+				>
+					Project Presentation
+				</Typography>
 
-          <video controls width="100%" height="auto">
-            <source src={videoUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-      </div>
-    </>
-  );
+				<Typography
+					align="center"
+					component="a"
+					mx="auto"
+					href="https://github.com/hn275/distributed-storage"
+					target="_blank"
+					sx={{
+						fontWeight: "normal",
+						textDecoration: "underline",
+						fontStyle: "italic",
+						color: "blue",
+						fontSize: 15,
+					}}
+				>
+					Github Repository: distributed-storage
+				</Typography>
+
+				<Box mx="auto" width="80%">
+					<video controls width="100%" height="auto">
+						<source src={videoUrl} type="video/mp4" />
+						Your browser does not support the video tag.
+					</video>
+				</Box>
+			</Box>
+		</main>
+	);
 }
