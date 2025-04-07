857bec74cc75324c1ea02ea0537cc26fb2a4fe4a
Hal <haln_01@proton.me>
2025-04-06 18:09:59 -0700

    published presentation page + formatting app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,64 @@
-import { CssBaseline } from '@mui/material';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.scss'
-import { HomePage } from './HomePage';
-import { NavBar } from './NavBar';
-import { ProposalPage } from './ProposalPage';
-import { BiWeekly } from './BiWeekly';
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import { HomePage } from "./HomePage";
+import { NavBar } from "./NavBar";
+import { ProposalPage } from "./ProposalPage";
+import { BiWeekly } from "./BiWeekly";
+import { Presentation } from "./Presentation";
+import { AppPageType, makePath } from "./utils/common";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <CssBaseline />
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route path={makePath(AppPageType.HOME)} element={<HomePage />} />
 
-	return (
-		<BrowserRouter>
-			<div className="app-wrapper">
-				<CssBaseline />
-				<NavBar />
-				<div className="content">
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/proposal" element={<ProposalPage />} />
-						<Route
-							path="/bw1"
-							element={
-								<BiWeekly
-									header="Biweekly Update 1"
-									pdfPath="/biweekly-update-1.pdf" />
-							} />
-						<Route
-							path="/midterm-update"
-							element={
-								<BiWeekly
-									header="Midterm Update"
-									pdfPath="/midterm-update.pdf" />
-							} />
-						<Route
-							path="/bw3"
-							element={
-								<BiWeekly
-									header="Biweekly Update 3"
-									pdfPath="/biweekly-update-3.pdf" />
-							} />
-					</Routes>
-				</div>
-			</div>
-		</BrowserRouter>
-	)
+            <Route
+              path={makePath(AppPageType.PROPOSAL)}
+              element={<ProposalPage />}
+            />
+
+            <Route
+              path={makePath(AppPageType.BIWEEKLY1)}
+              element={
+                <BiWeekly
+                  header="Biweekly Update 1"
+                  pdfPath="/biweekly-update-1.pdf"
+                />
+              }
+            />
+            <Route
+              path={makePath(AppPageType.MIDTERM)}
+              element={
+                <BiWeekly
+                  header="Midterm Update"
+                  pdfPath="/midterm-update.pdf"
+                />
+              }
+            />
+            <Route
+              path={makePath(AppPageType.BIWEEKLY3)}
+              element={
+                <BiWeekly
+                  header="Biweekly Update 3"
+                  pdfPath="/biweekly-update-3.pdf"
+                />
+              }
+            />
+            <Route
+              path={makePath(AppPageType.PRESENTATION)}
+              element={<Presentation />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
